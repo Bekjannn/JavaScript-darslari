f1a1d7bd9565d98287395c0ea28cc403bf07bbad
Bekjannn <bekmahnudov@gmail.com>
2023-01-26 21:58:42 +0500

    homework

---
+++ main.js
@@ -0,0 +1,28 @@
+//  1- JavaScriptdagi data turlarini misollar orqali tushuntirib bering
+
+// 1.  Number:   1,  232,  3424,  -948,  3.14, ....;
+
+// 2.  String:   "hello world",  "wbacademy@gmail.com", ....;
+
+// 3.  Bigint:   123456789n,  65439876e, ....;
+
+// 4.  Boolean:  true / false ;
+
+// 5.  Null:  umuman hech qanday ma'lumot yo'q , Matematik jihatdan 0 ga teng.
+
+// 6.  Undefined:  o'zgaruvchi bor lekin qiymati yo'q ;
+
+// 7.  Symbols:  objectlar bilan birga ishlatilinadi ;
+
+// 8.  Objects:  
+
+// 2-String consoledan nima chiqadi ?
+
+let name = "ilya";
+
+console.log(`hello ${1}`);   // hello 1
+
+console.log(`hello ${"name"}`);  // hello name
+
+console.log(`hello ${name}`);  // hello ilya
+
